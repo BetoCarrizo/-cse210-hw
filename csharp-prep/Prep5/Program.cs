f221b27865c192269994e1234abc19a260cb507b
BetoCarrizo <beto.carrizo@hotmail.com>
2024-02-29 00:59:07 -0700

    Homework N5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("");
+        DisplayMessage();
+        string userName= PromptUserName();
+        int UserNumber=PromptUserNumber();
+        int UserSquare=SquareNumber ( UserNumber);
+        DisplayResult( UserSquare,  userName);
+        
+
+    }
+static void DisplayMessage()
+    {
+        Console.WriteLine(" Welcome to the program!");
+    }
+
+static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;   
+    }
+
+static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        string chose= Console.ReadLine();
+        int number = int.Parse(chose);
+        return number;
+    }
+static int SquareNumber(int number)
+    {
+        int square= number * number;
+        return square;
+    }
+
+static void DisplayResult(int square, string name)
+    {
+        Console.WriteLine($"Brother {name}, the square of your number is {square}");
     }
 }
