24231c97f81cd69441acc70b6b3012e83f838534
BetoCarrizo <beto.carrizo@hotmail.com>
2024-02-28 22:51:40 -0700

    finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        string port = Console.ReadLine();
+        int Magic_number = int.Parse(port);
+
+        Console.WriteLine("  What is your guess? ");
+        string Guess = Console.ReadLine();
+        int guess_number = int.Parse(Guess);
+
+        int guessCount=1;
+
+        while (guess_number != Magic_number)
+            {
+                if (guess_number>Magic_number)
+                    Console.WriteLine("Lower");
+                else if (guess_number<Magic_number)
+                    Console.WriteLine("Higher");
+                Console.WriteLine("What is your next guess?");
+                 Guess=Console.ReadLine();
+                 guess_number = int.Parse(Guess);
+                 guessCount++;
+            }
+
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine(guessCount);
     }
 }
