5b4ad63c4ba23e593165efafb6f2cff80842394b
BetoCarrizo <beto.carrizo@hotmail.com>
2024-03-27 07:40:06 -0600

    New Changes, Finish Learning 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
+
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
+    
+    {   Fraction acvFraction1 = new Fraction();
+        Console.WriteLine(acvFraction1.GetString());
+        Console.WriteLine(acvFraction1.GetResult());
+
+
+        Fraction acvFraction2 = new Fraction(5);
+        Console.WriteLine(acvFraction2.GetString());
+        Console.WriteLine(acvFraction2.GetResult());
+
+
+
+        Fraction acvFraction3 = new Fraction(5,6);
+        Console.WriteLine(acvFraction3.GetString());
+        Console.WriteLine(acvFraction3.GetResult());
+
     }
+
+
 }
+
+
+  
+
+
+
+
